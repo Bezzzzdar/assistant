370038ebb532dffec88aec13b3747999496fe820
Bezzzzdar <ivan.rastegaev.03@mail.ru>
2024-06-12 22:07:08 +0300

    app/app.py: add infinite loop for app

--- app/app.py
+++ app/app.py
@@ -1,13 +1,14 @@
-from modules.assistant_module import setup_voice_assistant, assistant_say, execute_command
-from modules.rec_module import record_and_recognize_audio
+from modules.assistant import VoiseAssistant, Owner
+from modules.record_and_recognize import record_and_recognize_audio
 
 if __name__ == "__main__":
 
-    setup_voice_assistant("Tayler", "male", "ru")
-
-    voice_input = record_and_recognize_audio()
-    execute_command(str(voice_input))
+    assistant = VoiseAssistant('Tyler', 'ru')
+    owner = Owner("Ivan", "ru")
     
-    
-
-    
+    while True:
+        voice_input = record_and_recognize_audio()
+        voice_input = voice_input.split(" ")
+        command = str(voice_input[0])
+        command_options = [str(input_part) for input_part in voice_input[1:len(voice_input)]]
+        VoiseAssistant.execute_command(assistant, command, command_options)

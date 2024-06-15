1ce01ab87c8e0a1229746036bee6dd4b0a656f60
Bezzzzdar <ivan.rastegaev.03@mail.ru>
2024-06-15 18:54:10 +0300

    app/app.py: fix misprints && new logic for split recorded voise

--- app/app.py
+++ app/app.py
@@ -1,14 +1,14 @@
-from modules.assistant import VoiseAssistant, Owner
-from modules.record_and_recognize import record_and_recognize_audio
+from modules.assistant import VoiceAssistant, Owner
+from modules.record_and_recognize import record_and_recognize_audio, split_phrase
 
 if __name__ == "__main__":
 
-    assistant = VoiseAssistant('Tyler', 'ru')
+    assistant = VoiceAssistant('Tyler', 'ru')
     owner = Owner("Ivan", "ru")
     
     while True:
         voice_input = record_and_recognize_audio()
-        voice_input = voice_input.split(" ")
-        command = str(voice_input[0])
-        command_options = [str(input_part) for input_part in voice_input[1:len(voice_input)]]
-        VoiseAssistant.execute_command(assistant, command, command_options)
+        commands_list, command_options_list = split_phrase(voice_input)
+        
+        for i in range(len(commands_list)):
+            VoiceAssistant.execute_command(assistant, commands_list[i], command_options_list[i])    

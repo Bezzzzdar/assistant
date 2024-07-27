d25e60d6200f7c4a96925d2c525832ebf194acfe
Bezzzzdar <158609280+Bezzzzdar@users.noreply.github.com>
2024-07-27 14:52:17 +0300

    Delete application/python/modules/nn_model. directory

--- application/python/modules/nn_model./add_songs.py
+++
@@ -1,9 +0,0 @@
-songs_from_vk = '''
-
- '''
-
-songs = [song for song in songs_from_vk.split('\n') if len(song) > 0 and ':' not in song]
-
-with open('songs.txt', 'a') as file:
-  for song in songs:
-    file.write(f'{song}\n')

8ced943d61f22fd3d7efaca96f5945c9dc1f9e03
Bezzzzdar <ivan.rastegaev.03@mail.ru>
2024-08-24 01:35:34 +0300

    application/python: app.py -> app_console.py

--- application/python/app_console.py
+++ application/python/app_console.py
@@ -2,10 +2,6 @@
 import sys
 import threading
 
-# Flask
-from flask import Flask, request, jsonify
-from flask_cors import CORS
-
 # Local modules
 from modules.assistant import VoiceAssistant
 from modules.owner import Owner
@@ -35,40 +31,8 @@ for i, arg in enumerate(argv):
 assistant = VoiceAssistant(assistant_name, language)
 owner = Owner(owner_name, language)
 
-app = Flask(__name__)
-CORS(app)
-
-@app.route('/api/data', methods=['POST'])
-def receive_data():
-    """Function for receiving data from gui app"""
-
-    data = request.get_json()
-    try:
-        if 'owner_name' in data:
-            owner.change_owner_name(data['owner_name'])
-        elif 'owner_language' in data:
-            owner.change_owner_language(data['owner_language'])
-        elif 'assistant_name' in data:
-            assistant.change_assistant_name(data['assistant_name'])
-        elif 'assistant_language' in data:
-            assistant.change_assistant_language(data['assistant_language'])
-    except Exception as e:
-        print(f"Error {e}")
-
-    print(f"Received data: {data}")
-    return jsonify({"status": "success", "data_received": data}), 200
-
-def run_flask():
-    """Function for start flask session"""
-
-    app.run(host='localhost', port=5000)
-
 def main():
     """Main function"""
-
-    flask_thread = threading.Thread(target=run_flask)
-    flask_thread.daemon = True
-    flask_thread.start()
 
     while True:
         voice_input = record_and_recognize_audio()
